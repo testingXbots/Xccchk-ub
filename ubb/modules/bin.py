5f8d7b507d2af86ff13c30760583724ae1fa5d3c
testingXbots <107467786+testingXbots@users.noreply.github.com>
2023-08-19 01:27:31 +0530

    Update bin.py

--- ubb/modules/bin.py
+++ ubb/modules/bin.py
@@ -6,7 +6,6 @@ from ubb.func import http
 
 @Ubot.on(events.NewMessage(pattern=r'\.bin'))
 async def srbin(event):
-    cancelled = False  # Flag to track if the message was deleted
     BIN = event.message.message[len('.bin '):]
     reply_msg = await event.get_reply_message()
     if reply_msg:
@@ -17,8 +16,8 @@ async def srbin(event):
         _res = await http.get(f'http://binchk-api.vercel.app/bin={_BIN}')
         res = _res.json()
 
-        # Post the message and get the message object
-        msg = await event.reply('Processing...')
+        # Send the "Processing..." message and get its object
+        msg = await event.client.send_message(event.input_chat, 'Processing...')
 
         # Wait for a very short duration before checking if the message still exists
         await asyncio.sleep(10)  # Adjust this value as needed
@@ -27,10 +26,9 @@ async def srbin(event):
         try:
             await event.client.get_messages(event.input_chat, ids=[msg.id])
         except:
-            cancelled = True  # Mark the message as cancelled
+            return  # Message was deleted, so no further action needed
 
-        if not cancelled:
-            new_msg = f'''
+        new_msg = f'''
 BIN: `{_BIN}`
 Brand⇢ **{res["brand"]}**
 Type⇢ **{res["type"]}**
@@ -42,8 +40,7 @@ Currency⇢ **{res["currency"]}**
 Country⇢ **{res["country"]}({res["code"]})**
 '''
 
-            # Edit the message with the new content
-            await msg.edit(new_msg)
+        # Edit the "Processing..." message with the new content
+        await msg.edit(new_msg)
     except:
-        if not cancelled:
-            await msg.edit('Failed to parse bin data from api')
+        await msg.edit('Failed to parse bin data from api')
