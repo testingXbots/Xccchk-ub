ed564c205d7fd11bb804d3efa05cc2f4b90f820f
testingXbots <107467786+testingXbots@users.noreply.github.com>
2023-08-18 23:50:40 +0530

    Update __main__.py

--- ubb/__main__.py
+++ ubb/__main__.py
@@ -4,15 +4,9 @@ import asyncio
 
 from ubb import Ubot
 from ubb.modules import ALL_MODULES
-from flask import Flask, request, Response
-
 
 for module_name in ALL_MODULES:
     imported_module = importlib.import_module(f"ubb.modules.{module_name}")
-
-app = Flask(__name__)
-loop = asyncio.get_event_loop()
-
 
 async def main():
     async with Ubot:
@@ -22,11 +16,5 @@ async def main():
               '(Press Ctrl+C to stop this)')
         await Ubot.run_until_disconnected()
 
-
-@app.route("/", methods=['GET'])
-def getMe():
-    return {"MyTelegramAccount": loop.run_until_complete(main())}
-
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    asyncio.run(main())
